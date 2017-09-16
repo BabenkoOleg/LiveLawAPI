3791e1d3a77fe5657e5d12bc4d454b9e7bd097f8
Kravchuk <majerkravchuk@gmail.com>
2017-09-17 01:59:29 +0300

    Fix CitiesFilter

--- app/models/concerns/cities_filter.rb
+++ app/models/concerns/cities_filter.rb
@@ -6,11 +6,11 @@ module CitiesFilter
       cities = City.all
 
       cities = cities.where(region_id: params[:region_id]) if params[:region_id]
-
       cities = cities.where('name ~* ?', "^#{params[:query]}") if params[:query]
-      cities = cities.limit(params[:limit] || 10).order('size DESC')
 
-      return cities
+      cities = cities.limit(params[:limit] || 10) unless params[:region_id]
+
+      return cities.order('size DESC')
     end
   end
 end
