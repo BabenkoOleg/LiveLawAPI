3cd0a3f79376d25c8fb3f336289a3de054c7ced0
Kravchuk <majerkravchuk@gmail.com>
2017-09-17 21:10:08 +0300

    Configure mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,23 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.default_url_options = { host: ENV['app_host'] }
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.yandex.ru',
+    port: 465,
+    domain: 'yandex.ru',
+    user_name: ENV['yandex_login'],
+    password: ENV['yandex_password'],
+    authentication: :plain,
+    enable_starttls_auto: true,
+    ssl: true,
+    tls: true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -81,3 +97,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+
