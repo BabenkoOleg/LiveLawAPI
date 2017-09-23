7e8c1d161a699ce21f7cd36d530536b21335aa26
Kravchuk <majerkravchuk.gmail.com>
2017-09-23 13:46:28 +0300

    Reset registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-      sessions: 'api/users/sessions',
-      registrations: 'api/user/registrations'
+      sessions: 'api/users/sessions'
     }
 
     resources :users, only: [:index, :show] do
