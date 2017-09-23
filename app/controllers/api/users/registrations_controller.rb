8c4995347b3d296c6b9013772edab0560a04f00d
Kravchuk <majerkravchuk.gmail.com>
2017-09-23 15:06:24 +0300

    Fix sign_up_params

--- app/controllers/api/users/registrations_controller.rb
+++ app/controllers/api/users/registrations_controller.rb
@@ -1,13 +1,8 @@
 class Api::Users::RegistrationsController < DeviseTokenAuth::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
 
-  # POST /resource
-  def create
-    super
-  end
+  protected
 
-  def configure_sign_up_params
-    devise_parameter_sanitizer.for(:sign_up)
-                              .push(*[:role, :first_name, :last_name, :phone])
+  def sign_up_params
+    params.permit(:email, :password, :role, :first_name, :last_name, :phone)
   end
 end
