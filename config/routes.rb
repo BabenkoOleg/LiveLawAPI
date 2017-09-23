8294a45e1ae43f618a1c67d3b9ff81704879ce3d
Kravchuk <majerkravchuk.gmail.com>
2017-09-23 14:41:52 +0300

    Fix registrations route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     mount_devise_token_auth_for 'User', at: 'auth', controllers: {
       sessions: 'api/users/sessions',
-      registrations: 'api/user/registrations'
+      registrations: 'api/users/registrations'
     }
 
     resources :users, only: [:index, :show] do
