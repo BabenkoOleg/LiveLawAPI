fabaad497741a7c18265d64a71326cd0c36ac033
Kravchuk <majerkravchuk@gmail.com>
2017-11-18 13:17:51 +0300

    Add city and category field to ChatSerializer

--- app/serializers/chat_serializer.rb
+++ app/serializers/chat_serializer.rb
@@ -1,9 +1,13 @@
 class ChatSerializer < ActiveModel::Serializer
-  attributes :id, :question, :status, :invited_at
+  attributes :id, :question, :status, :invited_at, :city, :category
   attribute :token, if: -> { should_render_chat_token }
   has_many :chat_messages
-end
 
-def should_render_chat_token
-  @instance_options[:chat_token]
+  def should_render_chat_token
+    @instance_options[:chat_token]
+  end
+
+  [:city, :category].each do |meth|
+    define_method(meth) { object.send(meth).name }
+  end
 end
