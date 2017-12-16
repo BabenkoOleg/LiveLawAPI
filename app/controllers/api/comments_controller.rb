0b76cef61e5b112f43c6b75350e9cee6aaa8db52
Kravchuk <majerkravchuk@gmail.com>
2017-12-16 15:04:44 +0300

    Implement the create action for comments

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -7,7 +7,35 @@ class Api::CommentsController < ApplicationController
     render json: comments, include: '*'
   end
 
+  # POST /questions/:id/comment
+  def create
+    if current_api_user.client? && current_api_user != @question.user
+      return render json: {
+        error: 'You can not comment on another user\'s question'
+      }, status: :forbidden
+    end
+
+    if params[:parent_id].present?
+      parent = Comment.find(params[:parent_id])
+      comment = parent.comments.new(comment_params)
+    else
+      comment = @question.comments.new(comment_params)
+    end
+
+    comment.user = current_api_user
+
+    if comment.save
+      render json: comment, status: :created
+    else
+      render json: comment.errors, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 
   def set_question
     @question = Question.find_by(id: params[:question_id])
