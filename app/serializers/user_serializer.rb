83aa6455a743c5b7a699edf6847748a9bb668c51
Kravchuk <majerkravchuk@gmail.com>
2017-12-24 12:41:53 +0300

    Fix city field in users

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -3,9 +3,10 @@ class UserSerializer < ActiveModel::Serializer
              :middle_name, :email_public, :phone, :experience, :qualification,
              :price, :university, :faculty, :date_of_graduation, :work, :staff,
              :dob, :balance, :online, :created_at, :updated_at, :online_time,
-             :login, :fax, :userdata, :extends, :full_name, :chat_status, :cities
+             :login, :fax, :userdata, :extends, :full_name, :chat_status
 
   attribute :chat_token, if: -> { should_render_chat_token }
+  belongs_to :city, serializer: CitySerializer
 
   def should_render_chat_token
     object.invited?
