486dbb766a1a2b88e820e93d0d19f4a9812d0740
Kravchuk <majerkravchuk@gmail.com>
2017-12-24 14:53:15 +0300

    Chenge order of questions

--- app/models/concerns/questions_filter.rb
+++ app/models/concerns/questions_filter.rb
@@ -3,7 +3,9 @@ module QuestionsFilter
 
   module ClassMethods
     def filter_by(params)
-      questions = Question.page(params[:page] || 1).per(params[:per_page] || 20)
+      questions =
+        Question.order("created_at DESC").page(params[:page] || 1).per(params[:per_page] || 20)
+
       questions = questions.where('(title||text) ~* ?', params[:query]) if params[:query]
       questions = questions.where(category_id: params[:category]) if params[:category]
 
